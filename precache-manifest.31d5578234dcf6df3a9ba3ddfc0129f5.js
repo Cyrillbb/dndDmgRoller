33ab1e71be1e585703b764a5079286bff97cc770
Fallopio <kibaryshnikov@yandex.ru>
2020-05-19 19:47:10 +0400

    Updates

---
+++ precache-manifest.31d5578234dcf6df3a9ba3ddfc0129f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a499cafdf707fa6528ae82bad3367d",
+    "url": "/dndDmgRoller/index.html"
+  },
+  {
+    "revision": "5f24704f299b4ea2128a",
+    "url": "/dndDmgRoller/static/css/main.a9916324.chunk.css"
+  },
+  {
+    "revision": "63f0374e28ebeb94a2f6",
+    "url": "/dndDmgRoller/static/js/2.1a189d07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dndDmgRoller/static/js/2.1a189d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f24704f299b4ea2128a",
+    "url": "/dndDmgRoller/static/js/main.43c39d5e.chunk.js"
+  },
+  {
+    "revision": "c38daaab2c95181053c0",
+    "url": "/dndDmgRoller/static/js/runtime-main.d3bbc68d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.75b684e16da5ec975a46ef4941be992c.js"
+  "/dndDmgRoller/precache-manifest.31d5578234dcf6df3a9ba3ddfc0129f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dndDmgRoller/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
