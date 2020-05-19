c0ac3651cf7f0edad22de05f517a9ce33b8c181b
Fallopio <kibaryshnikov@yandex.ru>
2020-05-19 19:45:17 +0400

    Updates

---
+++ precache-manifest.75b684e16da5ec975a46ef4941be992c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b5ac210ffeb8f4b52aea9f22780d70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c7c5a8afbcc2ef798f0",
+    "url": "/static/css/main.a9916324.chunk.css"
+  },
+  {
+    "revision": "63f0374e28ebeb94a2f6",
+    "url": "/static/js/2.1a189d07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a189d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7c5a8afbcc2ef798f0",
+    "url": "/static/js/main.019e80e7.chunk.js"
+  },
+  {
+    "revision": "f85880f3f465ea9265ca",
+    "url": "/static/js/runtime-main.7e559fa4.js"
+  }
+]);
