11b2e96ef6bfba6439137d6142580d49e5ee3639
Fallopio <kibaryshnikov@yandex.ru>
2020-05-20 18:17:21 +0400

    added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
         abMod: 0,
         profBon: 0,
         otherBon: 0,
-        wpnMax: 0,        
+        wpnMax: 0,
     })
     const [rollResult, setRollResult] = useState(0)
 
@@ -15,12 +15,12 @@ function App() {
         if (roll === 20) setRollResult('Critical hit!')
         else if (roll === 1) setRollResult('Critical miss!')
         else if (roll + state.abMod + state.profBon > 20) setRollResult(20)
-        else setRollResult(roll + state.abMod + state.profBon)        
+        else setRollResult(roll + state.abMod + state.profBon)
     }
 
     const handleDamageRoll = () => {
         let roll = Math.ceil(Math.random() * (state.wpnMax));
-        setRollResult(roll + state.abMod + state.otherBon)        
+        setRollResult(roll + state.abMod + state.otherBon)
     }
 
     return (
@@ -38,7 +38,7 @@ function App() {
                         onChange={e => setState({ ...state, profBon: parseInt(e.target.value) })} />
                     <label htmlFor="otherBon" className='text-light'>Enter ur other bonuses</label>
                     <input type="number" id='otherBon' className='form-control'
-                        onChange={e => setState({ ...state, otherBon: parseInt(e.target.value) })} />                    
+                        onChange={e => setState({ ...state, otherBon: parseInt(e.target.value) })} />
                     <label htmlFor="wpnMaxDmg" className='text-light'>Weapon max dmg</label>
                     <input type="number" id='wpnMaxDmgn' className='form-control'
                         onChange={e => setState({ ...state, wpnMax: parseInt(e.target.value) })} />
@@ -47,25 +47,33 @@ function App() {
                         Attack Roll
                     </button>
                     <button type="button" className="btn btn-primary m-1" data-toggle="modal" data-target="#Modal"
-                    onClick={handleDamageRoll}>
+                        onClick={handleDamageRoll}>
                         DMG roll
                     </button>
                     <button type="button" className="btn btn-primary m-1" data-toggle="modal" data-target="#Modal"
-                    onClick={() => setRollResult(Math.ceil(Math.random() * 10))}>
+                        onClick={() => setRollResult(Math.ceil(Math.random() * 10))}>
                         Roll d10
                     </button>
                     <button type="button" className="btn btn-primary m-1" data-toggle="modal" data-target="#Modal"
-                    onClick={() => setRollResult(Math.ceil(Math.random() * 20))}>
+                        onClick={() => setRollResult(Math.ceil(Math.random() * 20))}>
                         Roll d20
                     </button>
                     <button type="button" className="btn btn-primary m-1" data-toggle="modal" data-target="#Modal"
-                    onClick={() => setRollResult(Math.ceil(Math.random() * 8))}>
+                        onClick={() => setRollResult(Math.ceil(Math.random() * 8))}>
                         Roll d8
                     </button>
                     <button type="button" className="btn btn-primary m-1" data-toggle="modal" data-target="#Modal"
-                    onClick={() => setRollResult(Math.ceil(Math.random() * 6))}>
+                        onClick={() => setRollResult(Math.ceil(Math.random() * 6))}>
                         Roll d6
-                    </button>                    
+                    </button>
+                    <button type="button" className="btn btn-primary m-1" data-toggle="modal" data-target="#Modal"
+                        onClick={() => setRollResult(Math.ceil(Math.random() * 4))}>
+                        Roll d4
+                    </button>
+                    <button type="button" className="btn btn-primary m-1" data-toggle="modal" data-target="#Modal"
+                        onClick={() => setRollResult(Math.ceil(Math.random() * 100))}>
+                        Roll d100
+                    </button>
                 </div>
             </form>
             <Modal roll={rollResult} />
